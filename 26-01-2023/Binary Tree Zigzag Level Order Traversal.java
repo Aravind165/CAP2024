2635958bb1d98ac28651fe0d44134cc7086fc740
Naga Satya Aravind Surampudi <85440763+Aravind165@users.noreply.github.com>
2023-01-28 07:23:23 +0530

    Update Binary Tree Zigzag Level Order Traversal.java

--- 26-01-2023/Binary Tree Zigzag Level Order Traversal.java
+++ 26-01-2023/Binary Tree Zigzag Level Order Traversal.java
@@ -14,43 +14,38 @@
  * }
  */
 class Solution {
-    public HashMap<Integer,List<Integer>> tl(TreeNode root,int s,HashMap<Integer,List<Integer>> hm){
-        List<Integer> ls =new ArrayList<>();
-        
-        if(root!=null){
-            ls.add(root.val);
-            if(hm.containsKey(s)){
-                (hm.get(s)).addAll(ls);}
-            else{
-                hm.put(s,ls);
-            }
-           hm= tl(root.left,s+1,hm);
-           hm=tl(root.right,s+1,hm);
-        }
-        return hm;
-
-    }
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        HashMap<Integer,List<Integer>> l= new HashMap<>();
-        List<List<Integer>> ls1=new ArrayList<>();
-        int s=0;
+        List<List<Integer>> ls = new ArrayList<>();
         if(root==null){
-            return ls1;
+            return ls;
         }
-        l=tl(root,s,l);
-        int k=0;
-        for ( List<Integer> ls: l.values()){
-            if(k==0){
-                ls1.add(ls);
-                k=1;
+        Queue<TreeNode> que = new LinkedList<>();
+        que.add(root);
+        boolean flag=true;
+        while(!que.isEmpty()){
+            int n=que.size();
+            List<Integer> lis = new ArrayList<>();
+            for(int i=0;i<n;i++){
+                TreeNode ele = que.poll();
+                if(flag){
+                    lis.add(ele.val);
+                }
+                else{
+                    lis.add(0,ele.val);
+                }
+                if(ele.left!=null){
+                    que.add(ele.left);
+                }
+                if(ele.right!=null){
+                    que.add(ele.right);
+                }
+                
             }
-            else{
-                Collections.reverse(ls);
-                ls1.add(ls);
-                k=0;
-            }
+            flag=!flag;
+            ls.add(lis);
         }
-        return ls1;
+        return ls;
+        
     }
         
 }
